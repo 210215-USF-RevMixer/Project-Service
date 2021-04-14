7e9375b93d71bc8c778e7c5e5b810376b3b471ad
Steven Imas <StevenImas11@hotmail.com>
2021-04-13 20:26:30 -0400

    fixing projectDB tests

---
+++ Project-Service/ProjectTests/ProjectRepoDBTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTests
+{
+    public class ProjectRepoDBTests
+    {
+        private readonly DbContextOptions<ProjectDBContext> options;
+
+        public ProjectRepoDBTests()
+        {
+            options = new DbContextOptionsBuilder<ProjectDBContext>()
+            .UseSqlite("Filename=Test.db")
+            .Options;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
